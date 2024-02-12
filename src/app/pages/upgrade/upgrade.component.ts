60e1cf1fc9726b7437e89b13c553eab74d5930da
Fabian Adasme <fabian.adasmet@outlook.com>
2024-02-12 18:47:37 -0300

    Initial commit

---
+++ src/app/pages/upgrade/upgrade.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'upgrade-cmp',
+    moduleId: module.id,
+    template: ''
+  })  
+   // templateUrl: 'upgrade.component.html'
+
+
+export class UpgradeComponent implements OnInit{
+    ngOnInit(){
+    }
+}
